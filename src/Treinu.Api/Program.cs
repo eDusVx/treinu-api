1d91d88481d6e9c43b3a9bbab37aa89ffcd1e32d
eDusVx <eduardosvx@gmail.com>
2026-03-31 19:01:15 -0300

    feat: enable Swagger in all environments and update database connection string to production instance

--- src/Treinu.Api/Program.cs
+++ src/Treinu.Api/Program.cs
@@ -76,11 +76,8 @@ var app = builder.Build();
 
 app.UseExceptionHandler();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
